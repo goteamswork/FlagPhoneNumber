cdb2d53641ad4f51b697b52b34ee4cfe19c3a8ff
shiv pogra <49437548+sspogra@users.noreply.github.com>
2022-01-26 17:06:09 +0530

    Update FPNCountry.swift

--- Sources/FPNCountry.swift
+++ Sources/FPNCountry.swift
@@ -11,11 +11,10 @@ public struct FPNCountry: Equatable {
 		self.name = name
 		self.phoneCode = phoneCode
 		self.code = FPNCountryCode(rawValue: code)!
-        let bundle = "Resources/FlagKit.bundle/"
-		if let flag = UIImage(named: bundle + code + ".png", in: Bundle.FlagIcons, compatibleWith: nil) {
+		if let flag = UIImage(named:code, in: Bundle.FlagIcons, compatibleWith: nil) {
 			self.flag = flag
 		} else {
-			self.flag = UIImage(named: bundle + "unknown" + ".png", in: Bundle.FlagIcons, compatibleWith: nil)
+			self.flag = UIImage(named:"unknown", in: Bundle.FlagIcons, compatibleWith: nil)
 		}
 	}
 
